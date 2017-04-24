71784c98ffccfbc3344d6593b2ecd9b199a61d36
boybook <fangyizhou1997@qq.com>
2017-04-24 20:18:43 +0800

    TargetFinder!

---
+++ src/main/java/me/onebone/actaeon/target/AreaHaterTargetFinder.java
@@ -0,0 +1,34 @@
+package me.onebone.actaeon.target;
+
+import cn.nukkit.Player;
+import cn.nukkit.item.Item;
+import me.onebone.actaeon.entity.MovingEntity;
+
+public class AreaHaterTargetFinder extends TargetFinder {
+
+    private int radius;
+
+	public AreaHaterTargetFinder(MovingEntity entity, long interval, int radius){
+		super(entity, interval);
+        this.radius = radius;
+	}
+
+	protected void find() {
+        Player near = null;
+        double nearest = this.radius * this.radius;
+
+        for (Player player: this.getEntity().getLevel().getPlayers().values()) {
+            if (this.getEntity().distanceSquared(player) < nearest){
+                near = player;
+                nearest = this.getEntity().distance(player);
+            }
+        }
+
+        if (near != null) {
+            this.getEntity().setTarget(near, this.getEntity().getName());
+        } else {
+            //this.getEntity().getRoute().forceStop();
+            this.getEntity().setTarget(null, this.getEntity().getName());
+        }
+	}
+}
---
+++ src/main/java/me/onebone/actaeon/target/AreaPlayerHoldTargetFinder.java
@@ -0,0 +1,38 @@
+package me.onebone.actaeon.target;
+
+import cn.nukkit.Player;
+import cn.nukkit.item.Item;
+import me.onebone.actaeon.entity.MovingEntity;
+
+public class AreaPlayerHoldTargetFinder extends TargetFinder {
+
+	private Item item;
+    private int radius;
+
+	public AreaPlayerHoldTargetFinder(MovingEntity entity, long interval, Item item, int radius){
+		super(entity, interval);
+		this.item = item;
+        this.radius = radius;
+	}
+
+	protected void find() {
+        Player near = null;
+        double nearest = this.radius * this.radius;
+
+        for (Player player: this.getEntity().getLevel().getPlayers().values()) {
+            if (this.getEntity().distanceSquared(player) < nearest){
+                if (player.getInventory().getItemInHand().equals(this.item, false, false)){
+                    near = player;
+                    nearest = this.getEntity().distance(player);
+                }
+            }
+        }
+
+        if (near != null) {
+            this.getEntity().setTarget(near, this.getEntity().getName());
+        } else {
+            //this.getEntity().getRoute().forceStop();
+            this.getEntity().setTarget(null, this.getEntity().getName());
+        }
+	}
+}
